77c4986f67d8f5d84e1669c4ab672ddf13feecfc
srt1017 <omarfarouq107@gmail.com>
2023-05-14 17:57:14 -0500

    Built state, props, simple component, Will add more

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable prefer-const */
+import ListGroup from "./components/ListGroup";
+
+function App() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      ></ListGroup>
+    </div>
+  );
+}
+
+export default App;
