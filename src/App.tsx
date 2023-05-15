7c496f5c65a8e851a7a60c054fdace04e2b44889
srt1017 <omarfarouq107@gmail.com>
2023-05-15 18:11:29 -0500

    Added a button and a close-able alert to practice States

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
+import Button from "./components/Button";
+import Alert from "./components/alert";
+import { useState } from "react";
+
 /* eslint-disable prefer-const */
-import ListGroup from "./components/ListGroup";
-
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
+  const [alertVisible, setAlertVisible] = useState(false);
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      ></ListGroup>
+      {alertVisible && <Alert onClose={() => setAlertVisible(false)}>Hello</Alert>}
+      <Button color="danger" onClick={() => setAlertVisible(true)}>
+        Click me
+      </Button>
     </div>
   );
 }
